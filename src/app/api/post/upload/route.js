14d2aa62bf23f6891bb35e6a85e7fc09a05ca5de
05-rahul-vishwakarma <rahul930vishwakarma@gmail.com>
2024-04-28 11:16:47 +0530

    update

--- src/app/api/post/upload/route.js
+++ src/app/api/post/upload/route.js
@@ -9,7 +9,7 @@ export const POST = async (req) => {
   try {
     await connectToDB();
     const data = await req.formData();
-    console.log(data);   
+    console.log(data);
     let postPhoto = data.get("postPhoto");
     const bytes = await postPhoto.arrayBuffer();
     const buffer = Buffer.from(bytes);
@@ -27,16 +27,16 @@ export const POST = async (req) => {
 
     const newPost = await Post.create({
       creatorId: data.get("creatorId"),
-      username:data.get("username"),
+      username: data.get("username"),
       caption: data.get("caption"),
       tag: data.get("tag"),
       postPhoto: postPhoto,
     });
+    await newPost.save();
     return NextResponse.json({
       status: 200,
       message: "file uploaded successfully",
     });
-    await newPost.save();
   } catch (error) {
     console.log(error);
     return NextResponse.json({
